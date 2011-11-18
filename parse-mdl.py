19105ede1f04545fd83978d6b17ec8d36f69298b
Adam Jensen <acj@linuxguy.org>
2011-11-18 15:01:44 -0500

    Basic, working parser for Simulink/Stateflow chart stanzas.
    Wrapped parser init code into a function. Modified grammar to be more generic
    (e.g., to support space-delimited vectors as well as comma-delimited) and
    slightly easier to read. Introduced top-level "model" entity so that
    Stateflow stanzas appear as first-class elements in the parsed output.

--- parse-mdl.py
+++ parse-mdl.py
@@ -12,35 +12,6 @@ __author__ = 'Kjell Magne Fauske'
 __license__ = 'MIT'
 __version__ = '1.3'
 
-# A high level grammar of the Simulink mdl file format
-SIMULINK_BNF = """
-object {
-    members
-}
-members
-    variablename  value
-    object {
-        members
-    }
-variablename
-
-array
-    [ elements ]
-matrix
-    [elements ; elements]
-elements
-    value
-    elements , value
-value
-    string
-    doublequotedstring
-    float
-    integer
-    object
-    array
-    matrix
-"""
-
 import re
 import sys
 from pyparsing import *
@@ -59,41 +30,59 @@ def joinStrings(s,l,toks):
 	"""Join string split over multiple lines"""
 	return ["".join(toks)]
 
-# Define grammar
+def initializeParser():
+	# Define grammar
 
-# Parse double quoted strings. Ideally we should have used the simple statement:
-#    dblString = dblQuotedString.setParseAction( removeQuotes )
-# Unfortunately dblQuotedString does not handle special chars like \n \t,
-# so we have to use a custom regex instead.
-# See http://pyparsing.wikispaces.com/message/view/home/3778969 for details. 
-dblString = Regex(r'\"(?:\\\"|\\\\|[^"])*\"', re.MULTILINE)
-dblString.setParseAction( removeQuotes )
+	# Parse double quoted strings. Ideally we should have used the simple statement:
+	#    dblString = dblQuotedString.setParseAction( removeQuotes )
+	# Unfortunately dblQuotedString does not handle special chars like \n \t,
+	# so we have to use a custom regex instead.
+	# See http://pyparsing.wikispaces.com/message/view/home/3778969 for details. 
+	dblString = Regex(r'\"(?:\\\"|\\\\|[^"])*\"', re.MULTILINE)
+	dblString.setParseAction( removeQuotes )
 
 
-mdlNumber = Combine( Optional('-') + ( '0' | Word('123456789',nums) ) +
+	mdlNumber = Combine( Optional('-') + ( '0' | Word('123456789',nums) ) +
 					 Optional( '.' + Word(nums) ) +
 					 Optional( Word('eE',exact=1) + Word(nums+'+-',nums) ) )
-mdlObject = Forward()
-mdlName = Word('$'+'.'+'_'+alphas+nums)
-mdlValue = Forward()
-# Strings can be split over multiple lines
-mdlString = (dblString + Optional(OneOrMore(Suppress(LineEnd()) + LineStart()
-			+ dblString)))
-mdlElements = delimitedList( mdlValue )
-mdlArray = Group(Suppress('[') + Optional(mdlElements) + Suppress(']') )
-mdlMatrix =Group(Suppress('[') + (delimitedList(Group(mdlElements),';')) \
-			+ Suppress(']') )
-mdlValue << ( mdlNumber | mdlName| mdlString  | mdlArray | mdlMatrix )
-memberDef = Group( mdlName  + mdlValue ) | Group(mdlObject)
-mdlMembers = OneOrMore( memberDef)
-mdlObject << ( mdlName+Suppress('{') + Optional(mdlMembers) + Suppress('}') )
-mdlNumber.setParseAction( convertNumbers )
-mdlString.setParseAction(joinStrings)
-# Some mdl files from Mathworks start with a comment. Ignore all
-# lines that start with a #
-singleLineComment = Group("#" + restOfLine)
-mdlObject.ignore(singleLineComment)
-mdlparser = mdlObject
+
+	objectDef = Forward()
+	mdlName = Word('$'+'.'+'_'+alphas+nums)
+	mdlValue = Forward()
+	# Strings can be split over multiple lines
+	mdlString = (dblString + Optional(OneOrMore(Suppress(LineEnd()) +
+				LineStart() + dblString)))
+	# Stateflow vector elements are delimited by spaces, not commas. This
+	# rule handles both cases.
+	mdlArray = Group((Suppress('[') + mdlValue + OneOrMore(Optional(Suppress(Literal(','))) + mdlValue) + Suppress(']')))
+	mdlMatrix = Group(Suppress('[') +
+				delimitedList(Group(delimitedList(mdlValue, delim=',')), ';') +
+				Suppress(']'))
+	mdlValue << (mdlNumber | mdlName | mdlString | mdlArray | mdlMatrix)
+	memberDef = Group(mdlName + mdlValue) | Group(objectDef)
+	mdlMembers = OneOrMore(memberDef)
+	objectDef << (mdlName + Suppress('{') + Optional(mdlMembers) + Suppress('}'))
+	mdlObjects = OneOrMore(objectDef)
+	mdlModel = mdlName + Suppress('{') + Group(Optional(mdlObjects)) + Suppress('}')
+	mdlNumber.setParseAction(convertNumbers)
+	mdlString.setParseAction(joinStrings)
+	# Some mdl files from Mathworks start with a comment. Ignore all
+	# lines that start with a #
+	singleLineComment = Group("#" + restOfLine)
+	mdlModel.ignore(singleLineComment)
+	objectDef.ignore(singleLineComment)
+
+	return mdlModel
+
+def extractStateflow(blocks):
+	"From a set of blocks, extract those related to Stateflow charts"
+	sf_blocks = []
+	for b in blocks:
+		print b
+	print 'Top-level blocks seen: ' + str(len(blocks))
+	sys.exit(1)
+	#return sf_blocks
+	return blocks
 
 if __name__ == '__main__':
 	import pprint
@@ -105,6 +94,9 @@ if __name__ == '__main__':
 	inputfilename = sys.argv[1]
 	infile = open(inputfilename, 'r')
 	testdata = infile.read()
-	result = mdlparser.parseString(testdata)
+	mdlParser = initializeParser()
+	result = mdlParser.parseString(testdata)
+	sf_blocks = extractStateflow(result)
+	#pprint.pprint(sf_blocks)
 	pprint.pprint(result.asList())
 	infile.close()
